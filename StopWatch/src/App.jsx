eabb1235258376db7ea630dfa235d0a76ec503ee
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2024-04-23 17:22:56 +0530

    stop watch ui

--- StopWatch/src/App.jsx
+++ StopWatch/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleReset = () => {
+    clearInterval(intervalRefs.current);
+    setCount(0);
+  };
+
+  const intervalRefs = useRef();
+
+  const startHandler = () => {
+    intervalRefs.current = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+  };
+  const formatTime = (timeInSeconds) => {
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+    return `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+       <div className="container">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="card-header">Stop Watch</div>
+        <div className="card-body text-center">
+          <h5 className="card-title text-center my-4">{formatTime(count)}</h5>
+          <hr />
+          <button className="btn btn-primary mx-2" onClick={() => startHandler()}>
+            Start
+          </button>
+          <button
+            className="btn btn-primary mx-2"
+            onClick={() => clearInterval(intervalRefs.current)}
+          >
+            Pause
+          </button>
+          <button className="btn btn-primary" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <img src="./watch.jpeg" alt="" />
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
