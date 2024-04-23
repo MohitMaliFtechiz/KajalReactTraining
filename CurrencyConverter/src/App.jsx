6836012d37300c99cade15ca45ededc59881b57a
KajalKushwahaDeveloper <kajalkushwaha231096@gmail.com>
2024-04-23 12:08:42 +0530

    currency converter ui

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import CurrencyDropDown from "./CurrencyDropDown";
+import TextField from "@mui/material/TextField";
+import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   const [currencies, setcurrencies] = useState([]);
@@ -24,62 +27,73 @@ function App() {
     }
   };
 
-  const convertCurrency = async() => {
-    if (!amount) return
-    setConverting(true)
+  const convertCurrency = async () => {
+    if (!amount) return;
+    setConverting(true);
     try {
-      const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrencies}&to=${toCurrencies}`);
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrencies}&to=${toCurrencies}`
+      );
       const data = await res.json();
-  
+
       setConvertedAmount(data.rates[toCurrencies] + "" + toCurrencies);
     } catch (error) {
       console.log("error fetching", error);
-    }finally{
-      setConverting(false)
+    } finally {
+      setConverting(false);
     }
-  }
+  };
   useEffect(() => {
     fetchCurrencies();
   }, []);
   //  console.log("currencies", currencies)
   return (
     <>
-      <div>
-        <h2>Currency convertor</h2>
-        <div>
-          <label>Amount</label>
-          <input
-            type="number"
-            placeholder="Enter Here"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
-        {/* //from */}
-        <div>
-          <CurrencyDropDown
-            currencies={currencies}
-            title="from"
-            currency={fromCurrencies}
-            setcurrency={setFromCurrencies}
-            convertedAmount={convertedAmount}
-          />
-        </div>
+      <h2 style={{ textAlign: "center" }}>Currency convertor</h2>
+      <div className="currency_converter">
+        <div className="currency_container">
+          <div>
+            {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+            <label>Amount: </label>
+            <input
+              type="number"
+              placeholder="Enter Here"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+          </div>
+          {/* //from */}
+          <div className="currency_dropdown">
+            <div>
+              <CurrencyDropDown
+                currencies={currencies}
+                title="from"
+                currency={fromCurrencies}
+                setcurrency={setFromCurrencies}
+                convertedAmount={convertedAmount}
+              />
+            </div>
 
-        {/* //to */}
-        <div>
-          <CurrencyDropDown
-            currencies={currencies}
-            title="to"
-            currency={toCurrencies}
-            setcurrency={setToCurrencies}
-            convertedAmount={convertedAmount}
-          />
+            {/* //to */}
+            <div>
+              <CurrencyDropDown
+                currencies={currencies}
+                title="to"
+                currency={toCurrencies}
+                setcurrency={setToCurrencies}
+                convertedAmount={convertedAmount}
+              />
+            </div>
+          </div>
+          <p>currency converted : {convertedAmount} </p>
+          {/* <Button style={{color:'black'}} variant="outlined" onClick={convertCurrency}>convertor</Button> */}
+          <button style={{ color: "white" }} onClick={convertCurrency}>
+            convertor
+          </button>
         </div>
-
-        <p>currency converted : {convertedAmount} </p>
-
-        <button onClick={convertCurrency}>convertor</button>
+        <div>
+          <img src="./currency.jpeg" />
+        </div>
       </div>
     </>
   );
